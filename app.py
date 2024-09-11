4e338b32fd0a525b128784c3de9ca1b8260042f5
ThatPalAl <alekontox@gmail.com>
2024-09-11 18:54:55 +0200

    web app, charts on the subpage, instead of separate gui

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
 from map import generate_marathon_map
+import os
+from plotting import (
+    plot_race_times_hist,
+    plot_race_time_categories,
+    plot_age_vs_time,
+    histogram_pace,
+    pace_categories_perKM,
+    pace_pie_chart,
+    age_v_time,
+    pace_chart
+)
 
 app = Flask(__name__)
 
@@ -10,11 +21,21 @@ def home():
 @app.route('/my_marathons')
 def my_marathons():
     marathon_map = generate_marathon_map()  
+    
+    plot_race_times_hist()
+    plot_race_time_categories()
+    plot_age_vs_time()
+    histogram_pace()
+    pace_categories_perKM()
+    pace_pie_chart()
+    age_v_time()
+    pace_chart()
+
     return render_template('marathon_map.html', marathon_map=marathon_map)
 
 @app.route('/my_trainings')
 def my_trainings():
-    return "<h2>Trainings will be available soon!</h2>"
+    return "<h2>This part of the website is still in development!</h2>"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- plotting.py
+++ plotting.py
@@ -2,25 +2,22 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import seaborn as sns   
- 
-#lets dig into our dataset
+import os
+import matplotlib
+matplotlib.use('Agg')  #gui issues
+
+# Loading data
 df = pd.read_csv('./cleaned_polmaraton_results.csv')
-print('Lets check the dataset after the data corrections')
-print(df.head())
-print('Basic statistic operations to gain more insights from the dataset')
+#print(df.head())
 
 df['Czas_minutes'] = pd.to_timedelta(df['Czas']).dt.total_seconds() / 60
 df['czas_km_minutes'] = pd.to_timedelta('00:' + df['czas_km']).dt.total_seconds() / 60
 
+#print("Basic statistical summary:")
+#print(df.describe())
 
-print(df.head())
-
-print(df)
-print("Basic statistical summary:")
-print(df.describe())
-
-# separating results in groups, to present this data in various ways
-time_bins = [60, 75, 90, 105, 120, 135, 180] 
+# binning - performances
+time_bins = [60, 75, 90, 105, 120, 135, 180]
 time_labels = [
     '1. Best performances (0-75 min)',
     '2. Very good (75-90 min)',
@@ -32,8 +29,8 @@ time_labels = [
 
 df['Time_Category'] = pd.cut(df['Czas_minutes'], bins=time_bins, labels=time_labels, include_lowest=True)
 
-# time intervals for pace per km 
-pace_bins = [0, 4, 4.5, 5, 5.5, 6, 6.5, 10]  
+# Time intervals (pace per km)
+pace_bins = [0, 4, 4.5, 5, 5.5, 6, 6.5, 10]
 pace_labels = [
     '1. Best Performances (< 4 min/km)',
     '2. Very Good (4-4.5 min/km)',
@@ -46,102 +43,128 @@ pace_labels = [
 
 df['Pace_Category'] = pd.cut(df['czas_km_minutes'], bins=pace_bins, labels=pace_labels, include_lowest=True)
 
-#counting the number of participants ineach category
 time_category_counts = df['Time_Category'].value_counts().sort_index()
 pace_category_counts = df['Pace_Category'].value_counts().sort_index()
 
-# histogram of race times 
-plt.figure(figsize=(10, 6))
-plt.hist(df['Czas_minutes'], bins=50, color='blue', alpha=0.7)
-plt.title('Distribution of Race Times (minutes)')
-plt.xlabel('Time (minutes)')
-plt.ylabel('Frequency')
-plt.grid(True)
-plt.show()
 
-# bar chart of race times 
-plt.figure(figsize=(8, 6))
-time_category_counts.plot(kind='bar', color='skyblue', alpha=0.7)
-plt.title('Race Time Categories')
-plt.xlabel('Category')
-plt.ylabel('Number of Participants')
-plt.grid(True)
-plt.show()
+#if not os.path.exists('static/images'):
+#    os.makedirs('static/images')
 
-# pie chart of race times
-plt.figure(figsize=(8, 6))
-time_category_counts.plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette('coolwarm', 7))
-plt.title('Race Time Categories Distribution')
-plt.ylabel('')  
-plt.show()
+def save_plot(filename):
+    try:
+        path = os.path.join('static', 'images', filename)
+        #print(f"saving plot to {path}")  
+        plt.savefig(path, bbox_inches='tight')
+        plt.close()
+    except Exception as e:
+        print(f'Unable to save the plot image: {filename}')
 
-# case no.2 Km/H
-# histogram of pace per km
-plt.figure(figsize=(10, 6))
-plt.hist(df['czas_km_minutes'], bins=50, color='green', alpha=0.7)
-plt.title('Distribution of Average Pace per Kilometer')
-plt.xlabel('Pace per km')
-plt.ylabel('Frequency')
-plt.grid(True)
-plt.show()
+# Histogram - race times
+def plot_race_times_hist():
+    plt.figure(figsize=(10, 6))
+    plt.hist(df['Czas_minutes'], bins=50, color='blue', alpha=0.7)
+    plt.title('Race Times (minutes) - distribution')
+    plt.xlabel('Time (in minutes)')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    save_plot('race_times_histogram.png')
 
-# Bar chart of Pace per km Categories
-plt.figure(figsize=(8, 6))
-pace_category_counts.plot(kind='bar', color='lightgreen', alpha=0.7)
-plt.title('Pace per km Categories')
-plt.xlabel('Category')
-plt.ylabel('Number of Participants')
-plt.grid(True)
-plt.show()
+# Bar chart - race times
+def plot_race_time_categories():
+    time_category_counts = df['Time_Category'].value_counts().sort_index()
+    plt.figure(figsize=(8, 6))
+    time_category_counts.plot(kind='bar', color='skyblue', alpha=0.7)
+    plt.title('Race Time Categories')
+    plt.xlabel('Category')
+    plt.ylabel('Number of Participants')
+    plt.grid(True)
+    save_plot('race_time_categories.png')
 
-# pie chart of Pace per km Categories
-plt.figure(figsize=(8, 6))
-pace_category_counts.plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette('viridis', 7))
-plt.title('Pace per km Categories Distribution')
-plt.ylabel('') 
-plt.show()
+# Scatter plot - age vs race time
+def plot_age_vs_time():
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Wiek'], df['Czas_minutes'], alpha=0.5, color='purple')
+    plt.title('Age vs race time')
+    plt.xlabel('Age')
+    plt.ylabel('Time (in minutes)')
+    plt.grid(True)
+    save_plot('age_vs_time.png')
 
+# Histogram - pace per km
+def histogram_pace():
+    plt.figure(figsize=(10, 6))
+    plt.hist(df['czas_km_minutes'], bins=50, color='green', alpha=0.7)
+    plt.title('Average Pace per Kilometer')
+    plt.xlabel('pace per km')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    save_plot('histogram_pace.png')
 
-# age vs time simple plot
-plt.figure(figsize=(10, 6))
-plt.scatter(df['Wiek'], df['Czas_minutes'], alpha=0.5, color='purple')
-plt.title('Age vs. Race Time')
-plt.xlabel('Age')
-plt.ylabel('Time (minutes)')
-plt.grid(True)
-plt.show()
+# bar chart - pace per km categories
+def pace_categories_perKM():
+    plt.figure(figsize=(8, 6))
+    pace_category_counts.plot(kind='bar', color='lightgreen', alpha=0.7)
+    plt.title('Pace per km Categories')
+    plt.xlabel('Category')
+    plt.ylabel('Number of Participants')
+    plt.grid(True)
+    save_plot('pace_categories_perKM.png')
 
-#lets check how many participants we've got on each year we retrieved
-yearly_participation = df['Year'].value_counts().sort_index()
-plt.figure(figsize=(10, 6))
-yearly_participation.plot(kind='bar', color='orange', alpha=0.7)
-plt.title('Yearly Participation')
-plt.xlabel('Year')
-plt.ylabel('Number of Participants')
-plt.grid(True)
-plt.show()
+# Pie chart - pace per km categories
+def pace_pie_chart():
+    plt.figure(figsize=(8, 6))
+    pace_category_counts.plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette('viridis', 7))
+    plt.title('Pace per km Categories Distribution')
+    plt.ylabel('')
+    save_plot('pie_chart_pacePerKM.png')
 
+# Scatter plot - age vs time
+def age_v_time():
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Wiek'], df['Czas_minutes'], alpha=0.5, color='purple')
+    plt.title('Age vs. Race Time')
+    plt.xlabel('Age')
+    plt.ylabel('Time (minutes)')
+    plt.grid(True)
+    save_plot('age_v_time.png')
 
-plt.figure(figsize=(8, 6))
-df['Time_Category'].value_counts().plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette('coolwarm', 6), startangle=90)
-plt.title('Race Time Categories Distribution')
-plt.ylabel('')  # Hide y-label to make chart cleaner
-plt.show()
+# Bar chart - yearly participation 
+def participants_count():
+    yearly_participation = df['Year'].value_counts().sort_index()
+    plt.figure(figsize=(10, 6))
+    yearly_participation.plot(kind='bar', color='orange', alpha=0.7)
+    plt.title('Yearly Participation')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Participants')
+    plt.grid(True)
+    save_plot('participants_count.png')
 
-# age vs time - Area chart
-plt.figure(figsize=(10, 6))
-df.groupby('Wiek')['Czas_minutes'].mean().plot(kind='area', color='lightblue', alpha=0.6)
-plt.title('Average Time per Age Group')
-plt.xlabel('Age')
-plt.ylabel('Average Time (minutes)')
-plt.grid(True)
-plt.show()
+# Area chart - average time per age group
+def age_vs_time():
+    plt.figure(figsize=(10, 6))
+    df.groupby('Wiek')['Czas_minutes'].mean().plot(kind='area', color='lightblue', alpha=0.6)
+    plt.title('Average Time per Age Group')
+    plt.xlabel('Age')
+    plt.ylabel('Average Time (minutes)')
+    plt.grid(True)
+    save_plot('age_v_time_area.png')
 
-# Pace - age, bar chart 
-plt.figure(figsize=(10, 6))
-df.groupby('Wiek')['czas_km_minutes'].mean().plot(kind='bar', color='lightgreen', alpha=0.7)
-plt.title('Average Pace per Age Group')
-plt.xlabel('Age')
-plt.ylabel('Average Pace (minutes per km)')
-plt.grid(True)
-plt.show()
+# Bar chart - pace per age group
+def pace_chart():
+    plt.figure(figsize=(10, 6))
+    df.groupby('Wiek')['czas_km_minutes'].mean().plot(kind='bar', color='lightgreen', alpha=0.7)
+    plt.title('Average Pace per Age Group')
+    plt.xlabel('Age')
+    plt.ylabel('Average Pace (minutes per km)')
+    plt.grid(True)
+    save_plot('pace_chart.png')
+
+#generating:
+plot_race_times_hist()
+plot_race_time_categories()
+plot_age_vs_time()
+histogram_pace()
+pace_categories_perKM()
+pace_pie_chart()
+age_v_time()
+pace_chart()
